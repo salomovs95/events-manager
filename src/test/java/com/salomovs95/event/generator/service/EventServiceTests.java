bb5269857cf1e9d62b58c06edf2a2558863cb71d
Salomao S. <souza95salomao@gmail.com>
2025-02-18 03:38:52 +0000

    chore: project setup

---
+++ src/test/java/com/salomovs95/event/generator/service/EventServiceTests.java
@@ -0,0 +1,13 @@
+package com.salomovs95.event.generator.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
