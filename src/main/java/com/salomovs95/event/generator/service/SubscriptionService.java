36b3f1cc5995a0365d9525a2328318e70f945e08
Salomao S. <souza95salomao@gmail.com>
2025-02-19 23:31:34 +0000

    feat: adds support for subscription ranking reports

--- src/main/java/com/salomovs95/event/generator/service/SubscriptionService.java
+++ src/main/java/com/salomovs95/event/generator/service/SubscriptionService.java
@@ -1,14 +1,19 @@
 package com.salomovs95.event.generator.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.salomovs95.event.generator.dto.SubscriptionRankingByUser;
+import com.salomovs95.event.generator.dto.SubscriptionRankingItem;
 import com.salomovs95.event.generator.dto.SubscriptionResponse;
 import com.salomovs95.event.generator.repository.SubscriptionRepository;
 import com.salomovs95.event.generator.entity.EventEntity;
 import com.salomovs95.event.generator.entity.SubscriptionEntity;
 import com.salomovs95.event.generator.entity.UserEntity;
 import com.salomovs95.event.generator.exception.EventNotFoundException;
+import com.salomovs95.event.generator.exception.SubscriptionNotFoundException;
 import com.salomovs95.event.generator.exception.SubscriptionAlreadyExistsException;
 import com.salomovs95.event.generator.repository.EventRepository;
 import com.salomovs95.event.generator.repository.UserRepository;
@@ -50,5 +55,22 @@ public class SubscriptionService {
     );
 
     return new SubscriptionResponse(subscription.getSubscriptionNumber(), referralLink);
+  }
+
+  public List<SubscriptionRankingItem> retrieveRanking(String prettyName) throws Exception {
+    EventEntity event = eventRepo.findByPrettyName(prettyName)
+      .orElseThrow(()->new EventNotFoundException(String.format("Event %s not found", prettyName)));
+    
+    return subscriptionRepo.generateRanking(event.getId());
+  }
+
+  public SubscriptionRankingByUser retrieveRankingByUser(String prettyName, Integer userId) throws Exception {
+    SubscriptionRankingItem ranking = retrieveRanking(prettyName)
+      .stream()
+      .filter(sub->sub.userId().equals(userId))
+      .findFirst()
+      .orElseThrow(()->new SubscriptionNotFoundException(String.format("No subscription in event %s was found for user %d", prettyName, userId)));
+    
+    return new SubscriptionRankingByUser(1, ranking);
   }
 }
