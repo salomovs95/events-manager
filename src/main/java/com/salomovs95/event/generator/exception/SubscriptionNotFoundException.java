4ec639102b4ad9bf8b7926c15882aa67a26ed0e8
Salomao S. <souza95salomao@gmail.com>
2025-02-19 23:29:32 +0000

    update: improves exception handling on ranking retrieval

---
+++ src/main/java/com/salomovs95/event/generator/exception/SubscriptionNotFoundException.java
@@ -0,0 +1,7 @@
+package com.salomovs95.event.generator.exception;
+
+public class SubscriptionNotFoundException extends RuntimeException {
+  public SubscriptionNotFoundException(String message) {
+    super(message);
+  }
+}
