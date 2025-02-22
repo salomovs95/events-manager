802a634c8a10511e4f9972adf296d238146e3e52
Salomao S. <souza95salomao@gmail.com>
2025-02-22 17:44:22 +0000

    update: implements ranking between events and improves exception handling

--- src/main/java/com/salomovs95/event/generator/controller/EventController.java
+++ src/main/java/com/salomovs95/event/generator/controller/EventController.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import io.swagger.v3.oas.annotations.Operation;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,43 +13,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.salomovs95.event.generator.dto.CreateEventDto;
-import com.salomovs95.event.generator.dto.ErrorMessage;
+import com.salomovs95.event.generator.dto.EventTopFiveItem;
 import com.salomovs95.event.generator.entity.EventEntity;
-import com.salomovs95.event.generator.exception.EventCreationException;
 import com.salomovs95.event.generator.service.EventService;
 
 @RestController @RequestMapping("/events")
 public class EventController {
   private final EventService eventService;
-  private Logger logg;
 
   public EventController(final EventService service) {
     this.eventService = service;
-    this.logg = LoggerFactory.getLogger(EventController.class);
   }
 
   @PostMapping("") @Operation(summary="Event Creation", description="Creates a new event.")
-  public ResponseEntity<?> createEvent(@RequestBody CreateEventDto dto) {
-    try {
+  public ResponseEntity<?> createEvent(@RequestBody CreateEventDto dto) throws Exception {
       EventEntity event = eventService.create(dto);
       return ResponseEntity.status(201).body(event);
-    } catch(EventCreationException e) {
-      logg.error("Cannot create event. " + e);
-      return ResponseEntity.status(400).body(new ErrorMessage(e.getMessage()));
-    } catch(Exception e) {
-      logg.error("Cannot create event. " + e);
-      return ResponseEntity.status(500).body(new ErrorMessage(e.getMessage()));
-    }
   }
 
   @GetMapping("") @Operation(summary="Events Retrieval", description="Retrieves a list of available events.")
-  public ResponseEntity<List<EventEntity>> listAllEvents() {
-    return ResponseEntity.status(200).body(eventService.findAll());
+  public ResponseEntity<List<EventTopFiveItem>> listTopEvents() {
+    return ResponseEntity.status(200).body(eventService.listTopFiveEvents());
   }
 
   @GetMapping("/{prettyName}") @Operation(summary="Event Retrieval", description="Retrieves information about a given event.")
-  public ResponseEntity<EventEntity> findEventByPrettyName(@PathVariable String prettyName) {
-    EventEntity event = eventService.findEvent(prettyName).orElse(null);
-    return ResponseEntity.status(event==null?404:200).body(event);
+  public ResponseEntity<EventEntity> findEventByPrettyName(@PathVariable String prettyName) throws Exception {
+    EventEntity event = eventService.findEvent(prettyName);
+    return ResponseEntity.status(200).body(event);
   }
 }
--- src/main/java/com/salomovs95/event/generator/service/EventService.java
+++ src/main/java/com/salomovs95/event/generator/service/EventService.java
@@ -1,18 +1,17 @@
 package com.salomovs95.event.generator.service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.salomovs95.event.generator.dto.CreateEventDto;
+import com.salomovs95.event.generator.dto.EventTopFiveItem;
 import com.salomovs95.event.generator.entity.EventEntity;
 import com.salomovs95.event.generator.exception.EventCreationException;
+import com.salomovs95.event.generator.exception.EventNotFoundException;
 import com.salomovs95.event.generator.repository.EventRepository;
 
 @Service
@@ -27,43 +26,40 @@ public class EventService {
     if (dto == null) throw new EventCreationException("Invalid data provided");
 
     final LocalDate startDate = dto.startDate(), endDate = dto.endDate();
-    System.out.println(
-      "Date is invalid: " + (
-      startDate.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli() <=
-      endDate.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
-    ));
-
-    if (startDate.isBefore(LocalDate.now()) || endDate.isBefore(LocalDate.now()) || startDate.isBefore(endDate)) {
-      throw new EventCreationException("Invalid event period");
-    }
+    if (startDate.atStartOfDay(ZoneId.systemDefault()).isAfter(endDate.atStartOfDay(ZoneId.systemDefault()))) throw new EventCreationException("Invalid event period");
 
     String prettyName = dto.title().toLowerCase().replaceAll(" ", "-");
     Optional<EventEntity> eventToBe = eventRepository.findByPrettyName(prettyName);
 
     if (eventToBe.isPresent()) return eventToBe.get();
-    
-    EventEntity event = eventRepository.save(new EventEntity(
-      null,
-      dto.title(),
-      prettyName,
-      dto.price(),
-      dto.location(),
-      startDate,
-      endDate
-    ));
+
+    EventEntity eventPrototype = new EventEntity();
+    eventPrototype.setPrice(dto.price());
+    eventPrototype.setTitle(dto.title());
+    eventPrototype.setLocation(dto.location());
+    eventPrototype.setPrettyName(prettyName);
+    eventPrototype.setStartDate(startDate);
+    eventPrototype.setEndDate(endDate);
+
+    return eventRepository.save(eventPrototype);
+  }
+
+  public EventEntity findEvent(Integer eventId) throws Exception {
+    EventEntity event = eventRepository.findById(eventId).orElseThrow(
+      ()->new EventNotFoundException(String.format("No event was found with id {%d}", eventId))
+    );
 
     return event;
   }
 
-  public Optional<EventEntity> findEvent(Integer eventId) {
-    return eventRepository.findById(eventId);
+  public EventEntity findEvent(String prettyName) throws Exception {
+    EventEntity event = eventRepository.findByPrettyName(prettyName).orElseThrow(
+      ()->new EventNotFoundException(String.format("No event was found with prettyName {%s}", prettyName))
+    );
+    return event;
   }
 
-  public Optional<EventEntity> findEvent(String prettyName) {
-    return eventRepository.findByPrettyName(prettyName);
-  }
-
-  public List<EventEntity> findAll() {
-    return eventRepository.findAll();
+  public List<EventTopFiveItem> listTopFiveEvents() {
+    return eventRepository.getTopFiveEvents();
   }
 }
