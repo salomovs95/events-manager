6d3c6148de2c3641199dfe63dbd4e5cfb7889ad8
Salomao S. <souza95salomao@gmail.com>
2025-03-06 21:44:57 +0000

    update: api documentation specifications

--- src/main/java/com/salomovs95/event/generator/dto/CreateEventDto.java
+++ src/main/java/com/salomovs95/event/generator/dto/CreateEventDto.java
@@ -2,10 +2,15 @@ package com.salomovs95.event.generator.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record CreateEventDto(
-  String title,
-  Integer price,
-  String location,
-  LocalDate startDate,
-  LocalDate endDate
+  @NotBlank @NotNull @Size(min=10) String title,
+  @NotNull @Min(1) Integer price,
+  @NotBlank @NotNull @Size(min=10) String location,
+  @NotNull LocalDate startDate,
+  @NotNull LocalDate endDate
 ) {}
--- src/main/java/com/salomovs95/event/generator/dto/CreateUserDto.java
+++ src/main/java/com/salomovs95/event/generator/dto/CreateUserDto.java
@@ -1,6 +1,11 @@
 package com.salomovs95.event.generator.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record CreateUserDto(
-  String username,
-  String email
+  @NotNull @NotBlank @Size(min=7) String username,
+  @NotNull @NotBlank @Size(min=7) @Email String email
 ) {}
